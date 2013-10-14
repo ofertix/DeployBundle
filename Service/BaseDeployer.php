8c86d326ba8fcf225ec92c372dbdf64eacda723d
Miquel Company Rodriguez <miquel.company@ofertix.com>
2013-10-14 16:27:44 +0200

    Get error log in exec using process

--- Service/BaseDeployer.php
+++ Service/BaseDeployer.php
@@ -668,7 +668,7 @@ abstract class BaseDeployer implements DeployerInterface
 
         if(!$process->isSuccessful())
         {
-            throw new \Exception('ERROR executing: ' . $command . "\n" .$process_output);
+            throw new \Exception('ERROR executing: ' . $command . "\n" .$process->getErrorOutput());
         }
 
         if(!empty($process_output))
